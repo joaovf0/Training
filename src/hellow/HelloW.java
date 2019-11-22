82cc7d1639c0570f386876bad9c0670215296011
Joao Gualberto Figueiredo (Retail) <JO40044425@wipro.com>
2019-11-22 09:54:15 -0300

    mudei o println

--- src/hellow/HelloW.java
+++ src/hellow/HelloW.java
@@ -16,7 +16,8 @@ public class HelloW {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello ");
+        System.out.println("hello caraai");
+        
     }
     
 }

bed46b459b757bdc80f05a0541117f08aaa6ef87
Joao Gualberto Figueiredo (Retail) <JO40044425@wipro.com>
2019-11-22 09:58:09 -0300

    mudei o println de novo

--- src/hellow/HelloW.java
+++ src/hellow/HelloW.java
@@ -16,7 +16,7 @@ public class HelloW {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hello caraai");
+        System.out.println("hello jão");
         
     }
     
